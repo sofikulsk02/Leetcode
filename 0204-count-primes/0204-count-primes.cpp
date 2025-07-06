97e08e3cbd25ca8e19364c7002e38711cb5844ba
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2025-07-06 12:22:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void fillSieve(vector<bool>&sieve){
-         int n=sieve.size()-1;
-         for(int i=2; i<=sqrt(n);i++){
-             for(int j=i*i;j<=n;j+=i){
-                sieve[j]=0;
-             }
-         }
+    void fillSeive(vector<bool>& seive,int n){
+        for(int i=2;i<=n;i++){
+            if(seive[i]==1){
+                for(int j=i*i;j<=n;j+=i){
+                    seive[j]=0;
+                }
+            }
+        }
     }
     int countPrimes(int n) {
-        if(n<=2) return 0;
-        n=n-1;
+       if(n<2) return 0;
+        n-=1;
+        vector<bool> seive(n+1,1);
+        seive[0]=seive[1]=0;
+        fillSeive(seive,n);
         int count=0;
-        vector<bool>sieve(n+1,1);
-        fillSieve(sieve);
-        sieve[0]=0;
-        sieve[1]=0;
         for(int i=2;i<=n;i++){
-           if(sieve[i]==1) count++;
+            if(seive[i]==1) count++;
         }
         return count;
     }
