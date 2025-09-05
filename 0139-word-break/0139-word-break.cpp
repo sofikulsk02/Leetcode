960292c7aca9f78e2e4814a4412b8aff92144466
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2025-09-05 19:53:07 +0530

    Time: 0 ms (100%), Space: 10.4 MB (91.88%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.length();
+        vector<bool>dp(n+1,false);
+        dp[n]=true;
+        for(int i=n-1;i>=0;i--){
+            for(auto w:wordDict){
+                if((i+w.size()<=n) && s.substr(i,w.size())==w){
+                    dp[i]=dp[i+w.size()];
+                }
+                if(dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+};
