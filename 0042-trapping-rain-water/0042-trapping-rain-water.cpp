3f6063abd68d1bde34eb41276cc2deedfa95f626
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2026-04-10 18:54:27 +0530

    Time: 0 ms (100%), Space: 27.2 MB (33.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    vector<int> leftMaxHeight(vector<int>& height,int &n){
-        vector<int>leftMax(n);
-        leftMax[0]=height[0];
+    vector<int> leftMaxEle(vector<int>& height,int &n){
+        vector<int>leftEle(n);
+        leftEle[0]=height[0];
         for(int i=1;i<n;i++){
-            leftMax[i]=max(leftMax[i-1],height[i]);
+            leftEle[i]=max(leftEle[i-1],height[i]);
         }
-        return leftMax;
+        return leftEle;
     }
-    vector<int> rightMaxHeight(vector<int>& height,int & n){
-        vector<int>rightMax(n);
-        rightMax[n-1]=height[n-1];
+    vector<int> rightMaxEle(vector<int>& height,int &n){
+        vector<int>rightEle(n);
+        rightEle[n-1]=height[n-1];
         for(int i=n-2;i>=0;i--){
-            rightMax[i]=max(rightMax[i+1],height[i]);
+            rightEle[i]=max(rightEle[i+1],height[i]);
         }
-        return rightMax;
+        return rightEle;
     }
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>leftMax=leftMaxHeight(height,n);
-        vector<int>rightMax=rightMaxHeight(height,n);
+        vector<int>leftMax=leftMaxEle(height,n);
+        vector<int>rightMax=rightMaxEle(height,n);
         int ans=0;
-        for(int i=0;i<height.size();i++){
-            int h=min(rightMax[i],leftMax[i])-height[i];
-            ans+=h;
+        for(int i=0;i<n;i++){
+            int w=min(leftMax[i],rightMax[i])-height[i];
+            ans+=w;
         }
         return ans;
     }
